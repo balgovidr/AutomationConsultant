c7c78e6278bade817c1629fe4ef992764d8acf78
balgovidr <balgovidr@hotmail.com>
2025-09-15 20:26:11 +0100

    Added login buttons

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -2,7 +2,7 @@
 
 export default function RootLayout({ children }) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center px-4">
+    <div className="h-full flex items-center justify-center px-4">
       <div className="w-full max-w-md bg-white rounded-lg shadow-sm border border-gray-200">
         {children}
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import React, { useEffect, useState } from "react";
 import { navigationLinks } from '@/constants/navBar';
+import { useAuth } from "@/providers/AuthProvider.jsx";
+import { useRouter } from "next/navigation";
+import Button from './Button';
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const { session } = useAuth();
+    const router = useRouter();
+
     // Create a click listener that closes the menu if clicked outside
     useEffect(() => {
-        const handleClickOutside = (event) => {
+        const handleClickOutside = () => {
             setIsMenuOpen(false);
         };
 
@@ -23,22 +29,40 @@ export default function NavBar() {
         }
     }, [isMenuOpen]);
 
+	function AuthButtons({size = "md", className}) {
+		if (session) {
+			return <Button variant="outline" size={size} onClick={() => router.push('/auth/logout')} className={className}>Logout</Button>
+		} else {
+			return (
+				<div className={'flex-row gap-2 items-center ' + className}>
+					<Button variant="secondary" size={size} onClick={() => router.push('/auth/signup')}>
+						Sign up
+					</Button>
+					<Button variant="primary" size={size} onClick={() => router.push('/auth/login')}>
+						Login
+					</Button>					
+				</div>
+			)
+		}
+	}
+
     return (
         <div>
-            <nav className="flex flex-row bg-gray-50 m-5 px-6 py-3.5 rounded-4xl text-blue-900 justify-between">
+            <nav className="flex flex-row bg-gray-50 m-5 px-6 py-3.5 rounded-4xl text-blue-900 justify-between items-center">
                 <a href="https://constructdigitally.com" target="_self" rel="home" aria-current="page">
-                    <h6 className="text-md md:text-lg font-medium font-sans">Construct Digitally</h6>
+                    <h6 className="text-md lg:text-lg font-medium font-sans">Construct Digitally</h6>
                 </a>
-                <button onClick={() => setIsMenuOpen(true)} aria-label="Menu" className='md:hidden'>
+                <button onClick={() => setIsMenuOpen(true)} aria-label="Menu" className='lg:hidden'>
                     <MenuIcon size={24} />
                 </button>
-                <div className='hidden md:flex flex-row gap-8 [&>*]:text-md [&>*]:font-medium [&>*]:font-sans [&>*]:text-blue-900'>
+                <div className='hidden lg:flex flex-row gap-8 [&>*]:text-md [&>*]:font-medium [&>*]:font-sans [&>*]:text-blue-900'>
                     {navigationLinks.map((link) => (
                         <a key={link.name} href={link.href} target="_blank" rel="noopener noreferrer">
                             {link.name}
                         </a>
                     ))}
                 </div>
+				<AuthButtons className="hidden lg:flex" />
             </nav>
             {isMenuOpen && (
                 <div className="absolute top-0 left-0 bg-white p-5 min-w-full min-h-screen z-10 flex flex-col gap-8">
@@ -49,6 +73,7 @@ export default function NavBar() {
                                 {link.name}
                             </a>
                         ))}
+						<AuthButtons size="lg" className="flex" />
                     </div>
                 </div>
             )}
